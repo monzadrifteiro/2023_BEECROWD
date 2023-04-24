52889558b9d687ac3ebb6cd19381976c20c81afb
Lucas de Lima <93940387+monzadrifteiro@users.noreply.github.com>
2023-04-24 11:27:27 -0300

    Add files via upload

---
+++ 1075.cs
@@ -0,0 +1,16 @@
+using System; 
+public class Program {
+  public static void Main(string[] args) {
+
+    int valor = int.Parse(Console.ReadLine());
+
+    for(int i = 1; i <= 10000; i++) {
+
+      if(i % valor == 2) {
+        Console.WriteLine(i);
+      }
+      
+    }
+    
+  }
+}
